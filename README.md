f5b1db4284cb98cda46db169c1625bc0798b76da
Saniul <saniulmallik@techis.io>
2023-01-12 20:36:58 +0530

    simon started

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # WD-Cource
 work of WD course of UDEMY by ANGELA YU
 # REACT -> CODESANDBOX
+
+
+terminal-->
+crtl+A
+CTRL+E
+OPT + pointer
---
+++ jQuery/Simon Game/game.js
@@ -0,0 +1,33 @@
+var buttonColours = ["red", "blue", "green", "yellow"]
+var gamePattern = []
+var userClickedPattern = []
+
+
+
+$(".btn").on("click", function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour)
+    playSound(userChosenColour)
+    animatePress(userChosenColour)
+})
+
+function nextSequence(){
+   var randomNumber = Math.floor(Math.random() *4)
+   var randomChosenColour = buttonColours[randomNumber]
+   gamePattern.push(randomChosenColour)
+
+   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+   playSound(randomChosenColour)
+}
+
+function playSound(name){
+   var audio = new Audio("./sounds/"+ name +".mp3")
+   audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+  }
